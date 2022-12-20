211c9ec410abf0a2760c0fbae615a8731cdb4e0d
anagreh <emam.anagreh@gmail.com>
2022-12-20 20:46:57 +0300

    create search form component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import "./styles/App.css";
 import PageHeading from "./components/pageHeading/PageHeading";
 import Breadcrumbs from "./components/breadcrumbs/Breadcrumbs";
+import SearchForm from "./components/searchForm/SearchForm";
 
 function App() {
   return (
     <div className="App">
       <PageHeading />
       <Breadcrumbs />
+      <SearchForm />
     </div>
   );
 }
